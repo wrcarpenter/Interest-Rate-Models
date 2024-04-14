2813150dd3b0b73f94dd9d9c6764fe1e8ec7be50
Will Carpenter <65867547+wrcarpenter@users.noreply.github.com>
2024-04-14 10:21:01 -0400

    Add files via upload

--- rate_model_engine.py
+++ rate_model_engine.py
@@ -28,8 +28,6 @@ swap    = lambda x: 0
 collar  = lambda x: 0
 bond    = lambda x: x
 
-# Create payoff functions then multiply them by a triangle array with zero and ones
-# the rates tree is already created/given for pricing 
 
 def cf_floor(rates, strike, delta, notion, cpn):
     
@@ -65,7 +63,7 @@ def cf_bond(rates, strike, delta, notion, cpn):
     Bond Cash flows
     '''
     cf  = np.zeros([len(rates), len(rates)])
-    # interatively fill up the array
+
     for col in range(0, len(cf)-1):
         for row in range(0, col+1):
             cf[row, col] = delta*notion*cpn/100  
@@ -73,8 +71,9 @@ def cf_bond(rates, strike, delta, notion, cpn):
     return cf
 
 def cf_swap(rates, strike, delta, notion, cpn):
-    cf  = np.zeros([len(rates), len(rates)])
-    # interatively fill up the array
+    
+    f  = np.zeros([len(rates), len(rates)])
+
     for col in range(0, len(cf)-1):
         for row in range(0, col+1):
             rate = rates[row,col]
@@ -83,7 +82,6 @@ def cf_swap(rates, strike, delta, notion, cpn):
     return cf
 
 
-# Print helper function 
 def display(arr):
   for i in arr:
     for j in i:
@@ -91,7 +89,7 @@ def display(arr):
     print() 
   print("\n")
 
-# Defining tree probabilities 
+
 def probTree(length):
     '''
     Generating a probability tree - assumed to be up/down 50%/50% for this project
@@ -102,15 +100,33 @@ def probTree(length):
     return(prob)
 
 
+def solver(theta, tree, zcb, i, sigma, delta):    
+    
+    # now we need to use all the variables and create a "final ZCB price"
+    for row in range(0, len(tree)):
+        if row == 0: 
+            tree[row, i] = tree[row, i-1] + theta*delta + sigma*math.sqrt(delta)
+        else:
+            tree[row, i] = tree[row-1, i-1] + theta*delta - sigma*math.sqrt(delta)
+            
+            
+    # now discount backwards to determine the price of the ZCB 
+    # PRICE BACKWARDS!!!!!
+    # return the price of the ZCB
+    
+    # need pricing tree?
+    
+    for col in reversed(range(0, i+1)):
+        # iterate backwards
+        
+    
+    
+    
+    
+    
+    return treePrice - zcb    
+    
 
-def solver(theta, zcb, tree):
-    
-    # calculate zcb price
-    # this is where you need to add pricing code
-    # this is where you use theta to create the rest of the tree and start to price backwards with the given rates
-    
-    # 
-    
 
 def calibrate(tree, zcb, i, sigma, delta)
 
@@ -123,17 +139,13 @@ def calibrate(tree, zcb, i, sigma, delta)
     miter = 1000
     
     # this should be a loop that assembles all theta and returns
-    theta = newton(solver, t0, args=(zcb, tree, sigma, delta))
+    theta = newton(solver, t0, args=(tree, zcb, i, sigma, delta))
     
     
-
-
-# Solve for theta
 def build(zcb, sigma, delta):
     
     tree  = np.zeros([len(zcb)+1, len(zcb)+1])
     theta = np.zeros([len(zcb), len(zcb)]) 
-    # Initialize the theta array
     
     # Initial Zero Coupon rate (monthly)
     tree[0,0] = ((1/zcb[0,0])**(1/(delta)))-1
@@ -141,19 +153,16 @@ def build(zcb, sigma, delta):
     for i in range(1, len(theta)):
         theta[i] = calibrate(tree, zcb[0,i], i, sigma, delta)
     
-    # Now we need to solve for the rest of the thetas via iteration
-                         
-# Create a tree
+    return theta
+    
 def rateTree(r0, theta, sigma, delta, model):
-    # create an interest rate tree based on a defined model for pricing 
+
     '''
     General Rate Model Tree Function
     
     Sigma (volatility) can be multi-dimentional for the full BDT model
     Theta is multi-dimentional
     '''
-
-    # generate an interest rate tree of size N+1 because the last period is payoff
 
     tree = np.zeros([len(theta)+2, len(theta)+2])
     # initialize tree
@@ -170,8 +179,7 @@ def rateTree(r0, theta, sigma, delta, model):
             tree[row, col] = tree[row-1, col] - 2*sigma*math.sqrt(delta)
     
     return tree
- 
-# Solve for pricing using the zero coupon tree                                             
+                                            
 def priceTree(rates, prob, cf, delta, payoff, notion):
     
     '''
@@ -242,6 +250,7 @@ if __name__ == "__main__":
     
     zcbs = np.array(zcbs.iloc[:,0:20])
     
+    # Calibrating the tree
     theta = build(zcbs, 0.015, 1/12)
     
     
@@ -249,6 +258,10 @@ if __name__ == "__main__":
     
             
 #%%
+
+i = 1
+for col in reversed(range(0, i+1)):
+    print(col)
     
 # Test cases
 # Slide 5 
