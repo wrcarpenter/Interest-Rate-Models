993b16d6cb3051bcf11b5576c0b94b986f9dd824
Will Carpenter <65867547+wrcarpenter@users.noreply.github.com>
2024-04-26 21:42:05 -0400

    Add files via upload

--- testing.py
+++ testing.py
@@ -18,65 +18,54 @@ zero_coupons = pd.read_csv('https://raw.githubusercontent.com/wrcarpenter/Intere
 zcbs = zero_coupons.loc[zero_coupons['Date']=='3/8/2024']
 zcbs = zcbs.drop("Date", axis=1)
 
-
 #%%
-zeros  = np.array(zcbs.iloc[:,0:48])
-cal    = model.build(zeros, 0.012, 1/12)
-tree   = model.rateTree(x[0], x[2], 0.012, 1/12, 'HL')
-cf     = model.cf_bond(tr, 5.00, 1/12, 1, 0.00)
-out    = model.priceTree(tr, 1/2, cf, 1/12, bond, 1)
-px     = out[0]
-ptree  = out[1] 
+# Build a large tree
+zeros     = np.array(zcbs.iloc[:,0:120])
+calibrate = model.build(zeros, 0.007, 1/12)
+tree      = model.rateTree(calibrate[0], calibrate[2], 0.007, 1/12)
 
+arr  = np.zeros([zeros.shape[1], 3])
 
-#%%
-def tree_monte_carlo(tree, paths):
+for i in range(0,len(arr)):
     
-    # return a dataframe
-    
+    # month
+    arr[i,0] = i+1
+    # Zero coupon price
+    arr[i,1] = zeros[0,i]
+    # use model
+    rate_tree = tree[:i+1, :i+1]
+    cash_flow = model.cf_bond(rate_tree, 0.00, 1/12, 1, 0.00) 
+    result    = model.priceTree(rate_tree, 1/2, cash_flow, 1/12, "bond", 1)
+    # Model zcb price    
+    arr[i, 2] = result[0]
+
+prices = pd.DataFrame(arr, columns=["Period", "ZCB Price", "Model Price"])
+
+
+chrt = chart_zcb_calibration(prices, 10, 5, "ZCB Calibration")
 
 
 #%%
-
-
-# Reading in interest rate tree data for testing 
-one_period_tree = np.array([[0.0168, 0.0433, np.nan], 
-                            [np.nan, 0.0120, np.nan], 
-                            [np.nan, np.nan, np.nan]]) 
-
-two_period_tree = np.array([[0.0168, 0.0433, 0.0638, np.nan], 
-                            [np.nan, 0.0120, 0.0361, np.nan], 
-                            [np.nan, np.nan, 0.0083, np.nan], 
-                            [np.nan, np.nan, np.nan, np.nan]])
-
-largeTree = pd.read_csv("https://raw.githubusercontent.com/wrcarpenter/Fixed-Income-Valuation/main/Data/testTree.csv", header=0).values
-
-# Testing a cash flow generation
-# 1/2 delta is semi annual 
-flr = cf_floor(two_period_tree, 1.00, 1/2, 100, 5.00)
-cp  = cf_cap(two_period_tree, 1.00, 1/2, 100, 5.00)
-bd  = cf_bond(two_period_tree, 1.00, 1/2, 100, 5.00)
-
-#%%
+# Charting example       
+def chart_zcb_calibration(arr, w, l, title):
     
-# Test cases
-# Slide 5 
-# prob  = probTree(4)
-# pu    = prob[1,1]
-# cf    = cfTree(one_period_tree, 5.00, delta, 1, ) 
-# delta = 1/2
+    x1 = np.array(arr['Period'])
+    y1 = np.array(arr['ZCB Price'])
+    y2 = np.array(arr['Model Price'])
+    
+    fig,ax = plt.subplots(figsize=(w,l))
+    ax.set_xticks(np.arange(1, len(arr)+10, 15))
+    ax.set_yticks(np.arange(0,1,0.05))
+    
+    
+    ax.set_title(title, fontsize="large")
+    ax.set_ylabel('Price', fontsize="large")
+    ax.set_xlabel('Months', fontsize="large")
+    
+   
+    plt.scatter(x1, y1, color='blue', marker='s', label="Market Price")
+    plt.plot(x1, y2, color='green',label='Model Price')
+    
+    plt.legend(loc='upper right', fontsize='large')
 
-# price = priceTree(one_period_tree, prob, cf, delta, bond, 1)
-
-# # Slide 7
-# prob  = probTree(5) 
-# delta = 1/2
-
-# price = priceTree(two_period_tree, prob, delta, cf, bond, 1)
-
-# prob  = probTree(len(rateTree))
-# cf = cfTree(rateTree, 0, 1/2, 100, 0.02, bond_cf)
-# delta = 1/2
-
-# price = priceTree(rateTree, prob, cf, delta, bond, 100)
 
